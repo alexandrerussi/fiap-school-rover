7fb1926b2f6fbd84aa530fbb8956381be818ffe8
alexandrerussi <alexandrerussijunior@gmail.com>
2024-05-06 22:47:04 -0300

    atualização de grid e inserção de sorteio de rovers

--- src/components/DisplayPlayers.jsx
+++ src/components/DisplayPlayers.jsx
@@ -1,29 +1,59 @@
 // src/components/DisplayPlayers.jsx
-import React from 'react';
-import './DisplayPlayers.css'; // Importa o CSS para DisplayPlayers
+import React, { useState } from 'react';
+import './DisplayPlayers.css';
 import { useNavigate } from 'react-router-dom';
 import { usePlayers } from '../context/PlayerContext';
 
 function DisplayPlayers() {
     const { players, resetPlayers } = usePlayers();
-    const navigate = useNavigate(); // Hook para navegar entre as rotas
+    const navigate = useNavigate();
+    const [rovers, setRovers] = useState([
+        { nome: "95 Queen", turma: "7A" },
+        { nome: "Wall-E", turma: "7C" },
+        { nome: "Barco", turma: "7A" },
+        { nome: "BBB", turma: "7B" }
+    ]);
+    const [assignedRovers, setAssignedRovers] = useState([]);
 
     const handleBack = () => {
-        resetPlayers(); // Reseta os jogadores ao voltar
-        navigate('/'); // Navega de volta para a página de registro
+        resetPlayers();
+        navigate('/');
+    };
+
+    const handleAssignRover = () => {
+        if (assignedRovers.length < players.length) {
+            const remainingRovers = rovers.filter(rover => !assignedRovers.some(ar => ar.rover.nome === rover.nome));
+            const randomIndex = Math.floor(Math.random() * remainingRovers.length);
+            const selectedRover = remainingRovers[randomIndex];
+            const updatedAssignedRovers = [
+                ...assignedRovers,
+                { ...players[assignedRovers.length], rover: selectedRover }
+            ];
+            setAssignedRovers(updatedAssignedRovers);
+        }
     };
 
     return (
         <div>
-            <div className='players'>
-                {players.map((player, index) => (
-                    <div key={index}>
-                        <img src={player.photo} alt={`Foto de ${player.nickname}`} />
-                        <h2>{player.nickname}</h2>
-                    </div>
-                ))}
+            <div className='grid'>
+                <div className='players'>
+                    {players.map((player, index) => (
+                        <div key={index}>
+                            <img src={player.photo} alt={`Foto de ${player.nickname}`} />
+                            <h2>{player.nickname}</h2>
+                            {assignedRovers[index] && assignedRovers[index].rover ? (
+                                <p>{`${assignedRovers[index].rover.nome} (${assignedRovers[index].rover.turma})`}</p>
+                            ) : <p>Aguardando sorteio...</p>}
+                        </div>
+                    ))}
+                </div>
+                <div className='sort-reel'>
+                    <button onClick={handleAssignRover} disabled={assignedRovers.length >= players.length}>
+                        Sortear Rover
+                    </button>
+                </div>
             </div>
-            <button onClick={handleBack}>Voltar</button>  {/* Botão de voltar */}
+            <button onClick={handleBack}>Voltar</button>
         </div>
     );
 }
